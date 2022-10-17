cddb41cad68048f2168e0d1c834e63aabf252c9d
Njmilon <njmilon1@gmail.com>
2022-10-17 20:12:16 +0600

    validation added

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Typewriter } from 'react-simple-typewriter'
+
 
 const Home = () => {
     return (
-        <div className='bg-gradient-to-r from-purple-500 to-pink-500 w-full h-[100vh] relative'>
-            <h1 className='text-white font-extrabold text-9xl absolute top-44 left-32'>Welcome Home</h1>
+        <div className='bg-gradient-to-r from-purple-500 to-pink-500 w-full h-[100vh] flex items-center justify-center'>
+            <h1 className='text-white font-extrabold text-9xl'>
+                <Typewriter
+                    words={['Eat', 'Sleep', 'Code', 'Repeat', 'Passionate!']}
+                    loop={10}
+                    cursor
+                    cursorStyle='_'
+                    typeSpeed={90}
+                    deleteSpeed={60}
+                    delaySpeed={1000}
+                />
+            </h1>
         </div>
     );
 };
--- src/Register/Register.js
+++ src/Register/Register.js
@@ -1,36 +1,133 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from '../firebase/firebase.init';
+import { EyeOff, Eye } from 'react-hero-icon/solid';
+import Icon from 'react-hero-icon';
+
+
+// const auth = getAuth(app);
 
 const Register = () => {
+
+    //declare state
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleName = (e) => {
+        const nameValue = e.target.value;
+        setName(nameValue)
+    }
+
+    const handleEmail = (e) => {
+        const emailValue = e.target.value;
+        const validCheck = /\S+@\S+\.\S/.test(emailValue);//check pattern for email using regex
+        if (!validCheck) {
+            setError('Please recheck your email!!');
+            return;
+        }
+        setEmail(emailValue);
+        setError('')
+    }
+
+    const handlePassword = (e) => {
+        const passwordValue = e.target.value;
+        if (passwordValue.length === 0) {
+            setError('password must have atleast eight charaters with one uppercase, lowercase, digit and special character!!')
+            return;
+        }
+        if (!/(?=.{8,})/.test(passwordValue)) {
+            setError('password must be in 8 characters!')
+            return;
+        }
+        if (!/(?=.*?[A-Z])/.test(passwordValue)) {
+            setError('password must have atleast one uppercase letter')
+            return;
+        }
+        if (!/(?=.*?[a-z])/.test(passwordValue)) {
+            setError('password must have atleast one lowercase letter')
+            return;
+        }
+        if (!/(?=.*?[0-9])/.test(passwordValue)) {
+            setError('password must have atleast one digit')
+            return;
+        }
+        if (!/(?=.*?[#?!@$%^&*-])/.test(passwordValue)) {
+            setError('password must have atleast one special character')
+            return;
+        }
+        setPassword(passwordValue);
+        setError('')
+    }
+    // console.log(password);
+
+    // console.log(name, email, password);
+
+    //onSubmit handle method
+    // const handleRegister = (e) =>{
+    //     e.preventDefault();
+    //     const nameValue = e.target.name.value;
+    //     setName(nameValue);
+    //     const emailValue = e.target.email.value;
+    //     setEmail(emailValue);
+    //     const passwordValue = e.target.password.value;
+    //     setPassword(passwordValue);
+    //     console.log(name, email, password);
+    //    createUserWithEmailAndPassword(auth, email, password)
+    //    .then((userCredential) => {
+    //     const user = userCredential.user;
+    //     console.log(user);
+    //   })
+    //   .catch((error) => {
+    //     // const errorCode = error.code;
+    //     // const errorMessage = error.message;
+    //     console.error(error);
+    //   });
+    // }
+
+    const [showPass, setShowPass] = useState(false);
+
+    const handleshowpassword = () =>{
+        setShowPass(!showPass);
+    }
+
     return (
-        <div class="flex items-center justify-center min-h-screen bg-gray-100">
-            <div class="px-8 py-6 mx-4 mt-4 text-left bg-white shadow-lg md:w-1/3 lg:w-1/3 sm:w-1/3">
-                <h3 class="text-2xl font-bold text-center">Join us</h3>
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="px-8 py-6 mx-4 mt-4 text-left bg-white shadow-lg md:w-1/3 lg:w-1/3 sm:w-1/3">
+                <h3 className="text-2xl font-bold text-center">Join us</h3>
                 <form>
-                    <div class="mt-4">
+                    <div className="mt-4">
+                        <p className='text-red-600 text-center'><small>{error}</small></p>
                         <div>
-                            <label class="block" for="Name">Name</label>
-                            <input type="text" placeholder="Name"
-                                class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+                            <label for='userName' className="block" htmlFor="Name">Name</label>
+                            <input onBlur={handleName} name='name' id='userName' type="text" placeholder="Name"
+                                className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
                         </div>
-                        <div class="mt-4">
-                            <label class="block" for="email">Email</label>
-                            <input type="text" placeholder="Email"
-                                class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+                        <div className="mt-4">
+                            <label for='email' className="block" htmlFor="email">Email</label>
+                            <input onBlur={handleEmail} name='email' id='email' required type="email" placeholder="Email"
+                                className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
                         </div>
-                        <div class="mt-4">
-                            <label class="block">Password</label>
-                            <input type="password" placeholder="Password"
-                                class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+                        <div className="mt-4">
+                            <label for='password' className="block">Password</label>
+                            <div className='relative'>
+                                <input onBlur={handlePassword} name='password' id='password' required type={showPass ? "text" : "password"} placeholder="Password"
+                                    className="flex items-center w-full py-2 px-4 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+                                <label for='password' className='absolute top-2 right-2'>
+                                    <Icon onClick={handleshowpassword}  className="cursor-pointer" icon={showPass ? "EyeOff" : "eye"} />
+                                </label>
+                            </div>
                         </div>
-                        {/* <span class="text-xs text-red-400">Password must be same!</span> */}
-                        <div class="flex">
-                            <button class="w-full px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Create
+                        {/* <span className="text-xs text-red-400">Password must be same!</span> */}
+                        <div className="flex">
+                            <button type='submit' className="w-full px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Create
                                 Account</button>
                         </div>
-                        <div class="mt-6 text-grey-dark">
+                        <div className="mt-6 text-grey-dark">
                             Already have an account?
-                            <Link to='/login' class="text-blue-600 hover:underline" href="#">
+                            <Link to='/login' className="text-blue-600 hover:underline" href="#">
                                 Log in
                             </Link>
                         </div>
